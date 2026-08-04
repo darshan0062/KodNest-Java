3dc7b362b62ea58b5591cd3127bcca109e9802f2
M Darshan <mdarshan6362593330@gmail.com>
2026-08-04 11:40:12 +0530

    print statments

---
+++ track/practice/prints.java
@@ -0,0 +1,15 @@
+
+class prints {
+
+    public static void main(String[] args) {
+        System.out.println("monday");
+        System.out.print("tuesday");
+
+        System.out.println("wednesday");
+
+        System.out.println("thursday");
+        System.out.print("friday");
+        System.out.println("saturday");
+        System.out.println("sunday");
+    }
+}
