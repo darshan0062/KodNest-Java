b04d98dcd40f958ee4af81a5aacccc93e76c802c
M Darshan <mdarshan6362593330@gmail.com>
2026-08-05 11:19:12 +0530

    switch stmt

---
+++ track/practice/switchs.java
@@ -0,0 +1,21 @@
+// import java.util.*;
+
+public class switchs {
+
+    public static void main(String[] args) {
+        int num = 2;
+        switch (num) {
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+            case 3:
+                System.out.println("three");
+                break;
+            default:
+                System.out.println("invalid");
+        }
+    }
+}
