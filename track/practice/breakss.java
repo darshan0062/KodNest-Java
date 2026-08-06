7c8f6015f0b8f5f0e88e22040163b49512e8b6d8
M Darshan <mdarshan6362593330@gmail.com>
2026-08-06 12:51:14 +0530

    break stmt

---
+++ track/practice/breakss.java
@@ -0,0 +1,12 @@
+
+public class breakss {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
