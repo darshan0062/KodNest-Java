bf5ff5d6add22c4fc5bbd781956accbef481b084
M Darshan <mdarshan6362593330@gmail.com>
2026-09-09 17:29:33 +0530

    concat

---
+++ track/practice/module4 str/concat.java
@@ -0,0 +1,15 @@
+class concat{
+    public static void main(String[] args) {
+        String s1= "java";
+        String s2= "python";
+        String s3= s1+s2;
+        System.out.println(s3);
+        System.out.println(s1+s2);
+        String t1= new String ("anushka");
+        String t2= new String ("abc");
+        String res = t1.concat(t2);
+        System.out.println(res);
+        
+        
+    }
+}
