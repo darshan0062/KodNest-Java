ce9b8145d20725f45849db5563effe1230c46a7a
M Darshan <mdarshan6362593330@gmail.com>
2026-09-09 17:33:49 +0530

    uppercase

---
+++ track/practice/module4 str/touppercase.java
@@ -0,0 +1,11 @@
+
+
+class touppercase {
+    public static void main(String args[]) {
+        String str = "Java";
+        String res = str.toUpperCase();
+        System.out.println(res);
+        System.out.println(str);
+    }
+}
+
