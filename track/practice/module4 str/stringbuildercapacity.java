152e84f1db7536cd2020529d39b07a3f57a89437
M Darshan <mdarshan6362593330@gmail.com>
2026-09-10 17:01:07 +0530

    string buildr capacity

--- track/practice/module4 str/stringbuildercapacity.java
+++ track/practice/module4 str/stringbuildercapacity.java
@@ -1,10 +1,12 @@
 public class stringbuildercapacity {
 public static void main(String[] args) {
 StringBuilder sbr = new StringBuilder("Java");
-sbr.ensureCapacity(100);
+sbr.ensureCapacity(100);// by default capacity is 16 we increase to 100 using ensure 
 System.out.println(sbr.capacity());
 System.out.println(sbr);
 sbr.append("Program");
 System.out.println(sbr);
 }
-}
+}// if we give more than 16 char then jvm performs old capacity * 2 + 2 automatically
+
+ 
